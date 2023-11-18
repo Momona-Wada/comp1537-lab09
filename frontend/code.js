8a0f3a54a70094d4eb96f2ad5303c892fe27010e
Momona Wada <momona.9410@gmail.com>
2023-11-17 17:07:16 -0800

    Change url

--- frontend/code.js
+++ frontend/code.js
@@ -2,7 +2,9 @@ document.getElementById('weatherBtn').
     addEventListener('click', function() {
         var cityName = document.getElementById('cname').value;
         console.log(cityName);
-        fetch(`http://localhost:3000/?city=${cityName}`)
+        // let url = `http://localhost:3000`;
+        let url = `https://comp1537-lab09-momona.onrender.com`;
+        fetch(`${url}/?city=${cityName}`)
         .then(response =>  response.text())
         .then(data => {
             // This data is a dictionary form. How do I change?
@@ -11,7 +13,7 @@ document.getElementById('weatherBtn').
             resultDiv.innerHTML = `
                 <p>Temperature: ${parsedDta.temperature}</p>
                 <p>Description: ${parsedDta.description}</p>
-                <img src="https://openweathermap.org/img/wn/${parsedDta.icon}@2x.png"/>`;
+                <img src="https://openweathermap.org/img/wn/${parsedDta.icon}@2x.png"/>`
             
         })
         .catch(error => {
